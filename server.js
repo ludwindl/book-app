882ea73fefebf1199d6a1bea0903005ee83fb4b0
ludwindl <deleonantigua@gmail.com>
2019-08-14 17:47:40 -0700

    second trello card

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended:true}))
 app.use('/public', express.static('public'));
-
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
   response.render('pages/index');
 })
+
+app.post()
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}` ));
 
