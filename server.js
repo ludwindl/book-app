6112e0ba5cae761b02e50433c3e6124a7504863f
ludwindl <deleonantigua@gmail.com>
2019-08-15 18:40:43 -0700

    working on trello card 3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express =require('express');
 const app = express();
+const superagent = require('superagent');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,7 +14,19 @@ app.get('/', (request, response) => {
   response.render('pages/index');
 })
 
-app.post()
+function Book(info) {
+  this.title = info.title
+}
+
+app.post('/search', (request, response) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if (request.body.search[1] === 'author') {url += `inauthor: ${request.body.search[0]}`}
+  console.log('search: ',request.body.search[0]);
+  if (request.body.search[1] === 'title') {url += `intitle: ${request.body.search[0]}`}
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', {searchResults: results}));
+})
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}` ));
 
