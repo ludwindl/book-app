1fd0ecbd0c512c5efb7b999b6e220829391c6f20
ludwindl <deleonantigua@gmail.com>
2019-08-17 13:22:31 -0700

    displays image

--- server.js
+++ server.js
@@ -16,9 +16,15 @@ app.get('/', (request, response) => {
 
 function Book(info) {
   let httpRegex = /^(http:\/\/)/g;
-  this.title = info.title,
-  this.author = info.author
+  let placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = info.title ? info.title : 'No title available';
+  this.author = info.author ? info.author[0] : 'No author available';
+  this.isbn = info.industryIdentifiers ? `ISBN _13 ${info.industryIdentifiers[0].identifier}`: 'NO ISBN available';
+  this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail.replace(httpRegex, 'https://') : placeholderImage;
+  //this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
+  //this.description = info.description ? info.description : 'No description available';
 }
+
 
 
 
@@ -26,11 +32,11 @@ app.post('/search', createSearch);
 
 function createSearch (request, response){
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  console.log('request body', request.body);
-  console.log('actual search', request.body.search);
+  //console.log('request body', request.body);
+  //console.log('actual search', request.body.search);
   if (request.body.search[1] === 'title') {url += `intitle:${request.body.search[0]}`}
   if (request.body.search[1] === 'author') {url += `inauthor:${request.body.search[0]}`}
-  
+
   superagent.get(url)
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
     .then(results => response.render('pages/searches/show', {searchResults : results}))
